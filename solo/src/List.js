0d9daf1147b9579ec816db9360cc983a7669acaa
jonghyeon <jonghyeon37@naver.com>
2023-05-17 13:25:37 +0900

    update:Main-star

--- solo/src/List.js
+++ solo/src/List.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+import Product from './Product';
+import Category from './Category';
+import Exhibition from './Exhibition';
+import Brand from './Brand';
+import contents from './Main';
+
 function List() {
-    return (
-      <div>
-        List
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleHandler = () => {
+    setIsOn(!isOn);
+  };
+
+
+  return (
+    <div>
+      <div className='contents'>
+        <Product isOn={isOn} toggleHandler={toggleHandler} />
+        <Category isOn={isOn} toggleHandler={toggleHandler} />
+        <Exhibition isOn={isOn} toggleHandler={toggleHandler} />
+        <Brand isOn={isOn} toggleHandler={toggleHandler} />
       </div>
-    );
-  }
-  
-  export default List;
+      <div className='contents'>
+        <Product isOn={isOn} toggleHandler={toggleHandler} />
+        <Category isOn={isOn} toggleHandler={toggleHandler} />
+        <Exhibition isOn={isOn} toggleHandler={toggleHandler} />
+        <Brand isOn={isOn} toggleHandler={toggleHandler} />
+      </div>
+      <div className='contents'>
+        <Product isOn={isOn} toggleHandler={toggleHandler} />
+        <Category isOn={isOn} toggleHandler={toggleHandler} />
+        <Exhibition isOn={isOn} toggleHandler={toggleHandler} />
+        <Brand isOn={isOn} toggleHandler={toggleHandler} />
+      </div>
+      <div className='contents'>
+        <Product isOn={isOn} toggleHandler={toggleHandler} />
+        <Category isOn={isOn} toggleHandler={toggleHandler} />
+        <Exhibition isOn={isOn} toggleHandler={toggleHandler} />
+        <Brand isOn={isOn} toggleHandler={toggleHandler} />
+      </div>
+    </div>
+  );
+}
+
+export default List;
